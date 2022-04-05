e63cbb7dea0e19401a8b284b631fb9c76511dc5e
Smriti Shahi <smritishahi42@gmail.com>
2022-04-05 13:51:52 +0545

    'layoutlabchallenge'

--- app/src/main/java/com/example/hellotoastnew/MainActivity.java
+++ app/src/main/java/com/example/hellotoastnew/MainActivity.java
@@ -9,26 +9,25 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    private int mCount = 0;
+    private int mCount=0;
     private TextView mShowCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mShowCount = (TextView) findViewById(R.id.show_count);
+        mShowCount=(TextView)findViewById(R.id.show_count);
     }
 
     public void showToast(View view) {
-        Toast toast = Toast.makeText(this, R.string.toast_message,
-                Toast.LENGTH_SHORT);
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
         toast.show();
     }
 
     public void countUp(View view) {
         mCount++;
-        if (mShowCount != null)
+        if(mShowCount!= null){
             mShowCount.setText(Integer.toString(mCount));
+        }
     }
 }
